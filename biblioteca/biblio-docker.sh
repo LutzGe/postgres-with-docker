891833fe381d1c6f377cb5ede145d9f953f3a82e
LutzGe <gabriel.lutz5@gmail.com>
2020-08-12 22:48:00 -0300

    é o último

--- biblioteca/biblio-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/$USER/dev/docker/postgres/biblioteca
-docker-compose up -d
-sleep 2
-docker exec -it biblioteca_db_1 psql -d biblioteca_dados
--- estoque/estoque-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/lutz/dev/docker/postgres/estoque
-docker-compose up -d
-sleep 2
-docker exec -it estoque_db_1 psql -d estoque
